a145195d0f4c6fc58373cddad35c1fe8ad149f1f
obloev <komronbekobloev@gmail.com>
2022-01-28 14:39:13 +0500

    markdown-to-html

--- handlers/cancel.py
+++ handlers/cancel.py
@@ -1,11 +1,10 @@
 from aiogram import types
 from aiogram.dispatcher import FSMContext
 
-from handlers.post import Post
 from loader import dp
 
 
-@dp.message_handler(commands=['cancel'], state=Post.get_post)
+@dp.message_handler(commands=['cancel'], state='*')
 async def start(message: types.Message, state: FSMContext):
     await state.finish()
     await message.reply('<b>❌ Canceled</b>')
