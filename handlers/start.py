4d9bbd305765bcb8bfea7e604a706085306941db
obloev <komronbekobloev@gmail.com>
2022-01-28 14:17:21 +0500

    error-start

--- handlers/start.py
+++ handlers/start.py
@@ -10,9 +10,9 @@ from utils.config import CHANNEL, JOINCHAT_URL, GROUP
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     user: types.User = message.from_user
-    await bot.send_message(GROUP, f'[{user.full_name}](tg://user?id={user.id}) launched  **THE BOT**')
     if await User.get_user(user.id) is None:
         await User.create_user()
+        await bot.send_message(GROUP, f'[{user.full_name}](tg://user?id={user.id}) launched  **THE BOT**')
     if not await check_membership(user.id):
         await message.answer(f'**Subscribe to the channel to use the bot [{CHANNEL}]({JOINCHAT_URL})**',
                              reply_markup=subscribe_keyboard())
